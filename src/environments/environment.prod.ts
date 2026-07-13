9f422144128e4ba974e5e6fe479fe658c175e4f9
Eslam Mohamed <eslama202002@gmail.com>
2026-07-13 12:22:49 +0300

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://med-agents-back.vercel.app/api',
-  
+  // apiUrl: 'https://med-agents-back.vercel.app/api',
+  apiUrl: 'http://localhost:5000/api',
+
 };
